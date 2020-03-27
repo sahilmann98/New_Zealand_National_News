b360bcac942b7ea79ed23871e33552813dd1b320
Sahil Mann <SM107@atcltd.atc.org.nz>
2020-03-27 18:41:31 +1300

    Add project files.

---
+++ New_Zealand_National_News/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using System.Collections.Generic;
+
+namespace New_Zealand_National_News
+{
+    [Activity(Label = "New_Zealand_National_News", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        RESThandler objRest;
+
+        ListView lstNews;
+        List<Item> tmpNewsList;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            lstNews = FindViewById<ListView>(Resource.Id.lstNews);
+
+            lstNews.ItemClick += OnLstNewsClick;
+
+            lstNews.FastScrollEnabled = true;
+            lstNews.ScrollingCacheEnabled = true;
+
+            LoadNew_Zealand_National_News();
+        }
+
+        void OnLstNewsClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var NewsItem = tmpNewsList[e.Position];
+            var NewsArticle = new Intent(this, typeof(Article));
+            NewsArticle.PutExtra("URL", NewsItem.Link);
+            StartActivity(NewsArticle);
+
+        }
+
+
+        public async void LoadNew_Zealand_National_News()
+        {
+            objRest = new RESThandler(@"http://rss.nzherald.co.nz/rss/xml/nzhrsscid_000000003.xml");
+            var Response = await objRest.ExecuteRequestAsync();
+
+            lstNews.Adapter = new DataAdapter(this, Response.Channel.Item);
+            tmpNewsList = Response.Channel.Item;
+        }
+
+    }
+}
